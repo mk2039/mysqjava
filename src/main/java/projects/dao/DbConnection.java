4d1eaeb8c394370a2a7ab6fcb4b92cdabc43ef73
monita <monitakhatri1989@gmail.com>
2025-04-18 12:00:22 -0700

    first commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,27 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+private static final String SCHEMA = "projects";
+private static final String USER = "projects";
+private static final String PASSWORD = "projects";
+private static final String HOST ="localhost";
+private static final int PORT = 3306 ;
+
+public static Connection getconnection() {
+	String url= String.format("jdbl:mysql://%s:%d/%s?user=5s&password=%s&useSSL=false",HOST,PORT,SCHEMA,USER,PASSWORD);
+	System.out.println("Connecting with url=" + url);
+	
+	try {
+	Connection conn = DriverManager.getConnection(url);
+	System.out.println("Successfully obtained connection!");
+	return conn;
+	}catch(SQLException e) { 
+	 throw new DbException(e);
+	}
+	
+  }
+}
